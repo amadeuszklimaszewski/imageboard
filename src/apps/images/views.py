6a4aa01c29408876d58515fad183235a70d11ce4
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-07-07 18:15:21 +0200

    RRemove duplicate code and docs comments

--- src/apps/images/views.py
+++ src/apps/images/views.py
@@ -14,7 +14,6 @@ from src.apps.images.services import ImageService
 
 class ImageViewSet(CreateModelMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Image.objects.all()
-    serializer_class = ImageOutputSerializer
     serializer_classes = {
         "Enterprise": EnterpriseImageOutputSerializer,
         "Premium": PremiumImageOutputSerializer,
@@ -22,23 +21,7 @@ class ImageViewSet(CreateModelMixin, viewsets.ReadOnlyModelViewSet):
     }
     service_class = ImageService
 
-    def get_serializer(self, *args, **kwargs):
-        serializer_class = self.get_serializer_class()
-        kwargs.setdefault("context", self.get_serializer_context())
-        return serializer_class(*args, **kwargs)
-
     def get_serializer_class(self):
-        """
-        Return the class to use for the serializer.
-        Defaults to using `self.serializer_class`.
-        You may want to override this if you need to provide different
-        serializations depending on the incoming request.
-        (Eg. admins get full serialization, others get basic serialization)
-        """
-        assert self.serializer_class is not None, (
-            "'%s' should either include a `serializer_class` attribute, "
-            "or override the `get_serializer_class()` method." % self.__class__.__name__
-        )
         account = self.request.user.user_account
         return self.serializer_classes.get(
             account.membership_type.name, self.serializer_classes["Basic"]
